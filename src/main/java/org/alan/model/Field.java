0eda9fd8e301cf80b97f157bbab207f502f6cd57
alankm194 <alankm194@gmail.com>
2023-02-11 05:00:51 +0000

    feat: create method to move vehicle in Field Class
    - vehicle in Field is marked by 1 instead of vehicle name for easier debugging

--- src/main/java/org/alan/model/Field.java
+++ src/main/java/org/alan/model/Field.java
@@ -5,19 +5,28 @@ import java.util.Arrays;
 public abstract class Field {
 
     private String[][] field;
-
+    private static final String VEHICLE_MARK = "1";
+    private static final String EMPTY_MARK = "0";
     public Field(int rowLength, int colLength){
         field = new String[rowLength][colLength];
         var row = new String[rowLength];
-        Arrays.fill(row, "0");
+        Arrays.fill(row, EMPTY_MARK);
         field[0] = row;
         for (int i=0; i < rowLength;i++) {
             field[i] = Arrays.copyOf(row, colLength);
         }
     }
 
+    public void moveVehicleInField(Vehicle vehicle) {
+        int x = vehicle.getCurrentPosX();
+        int y = vehicle.getCurrentPosY();
+        var move = vehicle.getMovementForCurrentDirection();
+        field[y][x] = EMPTY_MARK;
+        field[y + move.getMoveY()][x + move.getMoveX()] = VEHICLE_MARK;
+    }
+
     public void setVehicleInField(Vehicle vehicle, int xPos, int yPos) {
-        field[yPos][xPos] = vehicle.getName();
+        field[yPos][xPos] = VEHICLE_MARK;
         vehicle.setCurrentPosX(xPos);
         vehicle.setCurrentPosY(yPos);
     }
@@ -25,4 +34,6 @@ public abstract class Field {
     public String[][] getField() {
         return field;
     }
+
+
 }
--- src/test/java/org/alan/model/FieldTest.java
+++ src/test/java/org/alan/model/FieldTest.java
@@ -32,6 +32,29 @@ public class FieldTest {
         int xPos = 2;
         field.setVehicleInField(testVehicle, xPos, yPos);
         var fieldWithVehicle = field.getField();
-        assertEquals(testVehicle.getName(), fieldWithVehicle[yPos][xPos]);
+        assertEquals("1", fieldWithVehicle[yPos][xPos]);
+    }
+
+    @Test
+    public void moveVehicleInField() {
+        String[][] expected = create2dArrayAndFill(5, 5);
+        expected[2][3] = "1";
+        Field field = new RectangleField(5, 5);
+        Vehicle testVehicle = new MarsRover();
+        field.setVehicleInField(testVehicle, 2,2);
+        testVehicle.setCurrentFacingPosition(CardinalPoint.EAST);
+        field.moveVehicleInField(testVehicle);
+        assertArrayEquals(expected, field.getField());
+    }
+
+    private String[][] create2dArrayAndFill(int rowLength, int colLength) {
+        var row = new String[rowLength];
+        Arrays.fill(row, "0");
+        var filledArray = new String[rowLength][colLength];
+        filledArray[0] = row;
+        for (int i=0; i < rowLength;i++) {
+            filledArray[i] = Arrays.copyOf(row, colLength);
+        }
+        return filledArray;
     }
 }
