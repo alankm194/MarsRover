6289710776c436cd4a1f0d24f3f595026d36a9ce
alankm194 <alankm194@gmail.com>
2023-02-15 19:04:29 +0000

    test: Add new test that tests the console output of the 2d array

--- src/test/java/org/alan/controller/CompletePathControllerTest.java
+++ src/test/java/org/alan/controller/CompletePathControllerTest.java
@@ -10,15 +10,58 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.mockito.ArgumentCaptor;
-
-
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class CompletePathControllerTest {
+
+
+    private final static String[][] fieldArrayExpected1 =  {
+        {"-1", "-1", "-1",  " 0",  " 0",  " 0",  " 0", "-1", "-1", "-1" },
+        {"-1", "-1", " 0",  " 0",  " 0",  " 0",  " 0", " 0", "-1", "-1" },
+        {"-1", " 0", " 0",  " 0",  " 0",  " 0",  " 0", " 0", " 0", "-1" },
+        {" 0", " 0", " 2",  " 0",  " 0",  " 0",  " 0", " 0", " 0", " 0" },
+        {" 0", " 0", " 0",  " 0",  " 0",  " 0",  " 0", " 0", " 0", " 0" },
+        {" 0", " 0", " 0",  " 1",  " 0",  " 0",  " 0", " 0", " 0", " 0" },
+        {" 0", " 0", " 0",  " 0",  " 0",  " 0",  " 0", " 0", " 0", " 0" },
+        {"-1", " 0", " 0",  " 0",  " 0",  " 0",  " 0", " 0", " 0", "-1" },
+        {"-1", "-1", " 0",  " 0",  " 0",  " 0",  " 0", " 0", "-1", "-1" },
+        {"-1", "-1", "-1",  " 0",  " 0",  " 0",  " 0", "-1", "-1", "-1" },
+    };
+
+    private final static String[][] fieldArrayExpected2 = {
+        {" 0",  " 0",  " 0",  " 0"},
+        {" 0",  " 0",  " 0",  " 0"},
+        {" 0",  " 0",  " 0",  " 0"},
+        {" 0",  " 0",  " 0",  " 0"},
+        {" 0",  " 0",  " 0",  " 0"},
+        {" 0",  " 0",  " 1",  " 2"},
+        {" 0",  " 0",  " 0",  " 0"},
+        {" 0",  " 0",  " 0",  " 0"},
+    };
+
+    private final static String[][] fieldArrayExpected3 = {
+
+            {" 0",  " 0",  " 0",  " 0",  " 0",  " 0",  " 0"},
+            {" 0",  " 0",  " 0",  " 0",  " 0",  " 0",  " 0"},
+            {" 0",  " 0",  " 0",  " 0",  " 0",  " 0",  " 0"},
+            {" 0",  " 0",  " 0",  " 0",  " 0",  " 0",  " 0"},
+            {" 0",  " 0",  " 1",  " →",  " →",  " 2",  " 0"},
+            {" 0",  " 0",  " 0",  " 0",  " 0",  " 0",  " 0"},
+            {" 0",  " 0",  " 0",  " 0",  " 0",  " 0",  " 0"}
+    };
+
+    private static final Map<String, String[][]> fieldArrayExpectedMap = Map.of(
+            "fieldArrayExpected1", fieldArrayExpected1,
+            "fieldArrayExpected2", fieldArrayExpected2,
+            "fieldArrayExpected3", fieldArrayExpected3
+    );
+
 
     @Test
     public void testControllerForACompletePath() {
@@ -60,7 +103,7 @@ public class CompletePathControllerTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/completePathFullTest.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "/completePathFullTestForFinalLocation.csv", numLinesToSkip = 1)
     public void testStartControllerOutputsCorrectFinalLocation(int fieldRowLength,
                                     int fieldColLength,
                                     int vehicleStartX,
@@ -88,6 +131,41 @@ public class CompletePathControllerTest {
         verify(mockedOutput, times(1)).outputAnswerToConsole(argumentCaptor.capture(), any());
         assertEquals(expectedOutput, argumentCaptor.getValue());
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/completePathFullTestFor2dArrayPrintToConsole.csv", numLinesToSkip = 1)
+    public void testStartControllerOutputsCorrect2dArray(int fieldRowLength,
+                                                               int fieldColLength,
+                                                               int vehicleStartX,
+                                                               int vehicleStartY,
+                                                               String cardinalInitial,
+                                                               String movements,
+                                                               String vehicleType,
+                                                               String fieldType,
+                                                               String expectedKey) {
+
+        VehicleFieldDTO vehicleFieldDTO = new VehicleFieldDTO(
+                new int[]{fieldRowLength, fieldColLength},
+                new int[]{vehicleStartX, vehicleStartY},
+                cardinalInitial,
+                Arrays.asList(movements.split("")),
+                vehicleType,
+                fieldType
+        );
+
+        CompletePathController controller = new CompletePathController();
+        ConsoleOutput mockedOutput = mock(ConsoleOutput.class);
+        ArgumentCaptor<Object> argumentCaptor = ArgumentCaptor.forClass(Object.class);
+        controller.startController(vehicleFieldDTO, mockedOutput);
+
+        verify(mockedOutput, times(1)).outputFieldToConsole((String[][])argumentCaptor.capture(), any());
+        for(String[] f: (String[][]) argumentCaptor.getValue()){
+            System.out.println(Arrays.toString(f));
+        }
+
+        assertArrayEquals(fieldArrayExpectedMap.get(expectedKey), (String[][])argumentCaptor.getValue());
+    }
+
 
 
 
