e8253d61a6edb57381efec1fb37fa8df5502ab6b
alankm194 <alankm194@gmail.com>
2023-02-15 19:56:10 +0000

    test: Add tests that test the user output that gets print to console

---
+++ src/test/java/org/alan/view/ConsoleOutputTest.java
@@ -0,0 +1,45 @@
+package org.alan.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConsoleOutputTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        System.setProperty("line.separator", "\r\n");
+    }
+
+    @Test
+    public void outputAnswerToConsole() {
+        ConsoleOutput consoleOutput = new ConsoleOutput();
+        consoleOutput.outputAnswerToConsole("This is my test", "test message");
+        assertEquals("""
+                        test message
+                        This is my test""".replace("\n", "\r\n"),
+                outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void testOutputFieldToConsole() {
+        ConsoleOutput consoleOutput = new ConsoleOutput();
+        String[][] testArray = {
+                {" 0", " 1", "2"},
+                {"77", "56", " 44"},
+        };
+        consoleOutput.outputFieldToConsole(testArray, "test message");
+        assertEquals("test message" + "\r\n"
+                        + Arrays.toString(testArray[0]) + "\r\n"
+                        + Arrays.toString(testArray[1]),
+                outputStreamCaptor.toString().trim());
+    }
+
+}
